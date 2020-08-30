45477c204c067818f9151b29087965d6cbac651a
Tony Johnson <tony@tosp.net.au>
2020-08-30 20:29:20 +1000

    Cycle through inventory list in circular manner using arrow keys

--- client/src/inventory.rs
+++ client/src/inventory.rs
@@ -2,9 +2,13 @@ use yew::prelude::*;
 use super::model::inventory_model::*;
 use super::model::dictionary::*;
 use roguelike_common::*;
+use web_sys::{HtmlElement, HtmlCollection};
+use yew::utils::document;
+use wasm_bindgen::JsCast;
 
 pub struct Inventory {
     link: ComponentLink<Self>,
+    selected_item: i32,
     props: Props,
 }
 
@@ -17,6 +21,34 @@ pub struct Props {
 
 pub enum Msg {
     Pressed(KeyboardEvent),
+    GotFocus(FocusEvent),
+}
+
+impl Inventory {
+    fn cycle_list(&mut self, direction: i32) {
+        self.set_selected_item(self.selected_item, "");
+        let length = self.get_list_items().length() as i32;
+        let selected_idx = ((self.selected_item + direction) % length + length) % length;
+        self.set_selected_item(selected_idx, "li-selected");
+        self.selected_item = selected_idx;
+    }
+
+    fn get_list_items(&self) -> HtmlCollection {
+        document()
+            .get_elements_by_class_name("inventory-list")
+            .get_with_index(0)
+            .unwrap()
+            .dyn_into::<HtmlElement>()
+            .unwrap()
+            .children()
+    }
+
+    fn set_selected_item(&self, idx: i32, s: &str) {
+        match self.get_list_items().get_with_index(idx as u32) {
+            Some(item) => item.set_class_name(s),
+            None => (),
+        }
+    }
 }
 
 impl Component for Inventory {
@@ -26,6 +58,7 @@ impl Component for Inventory {
     fn create(props: Self::Properties, link: ComponentLink<Self>) -> Self {
         Self {
             link,
+            selected_item: 0,
             props,
         }
     }
@@ -43,8 +76,17 @@ impl Component for Inventory {
         match msg {
             Msg::Pressed(e) => {
                 if e.key_code() == KEY_ESC {
+                    self.set_selected_item(self.selected_item, "");
                     self.props.onkeydown_signal.emit(e);
+                } else if e.key_code() == KEY_DOWN {
+                    self.cycle_list(1);
+                } else if e.key_code() == KEY_UP {
+                    self.cycle_list(-1);
                 }
+            }
+            Msg::GotFocus(_e) => {
+                self.selected_item = 0;
+                self.set_selected_item(0, "li-selected");
             }
         }
         false
@@ -68,9 +110,10 @@ impl Component for Inventory {
                 class="inventory"
                 tabindex="0"
                 onkeydown=self.link.callback(Msg::Pressed)
+                onfocus= self.link.callback(Msg::GotFocus)
             >
                 <h3>{ "Inventory" }</h3>
-                <ul>
+                <ul class="inventory-list">
                 { for self.props.inventory.items
                     .iter()
                     .map(render_items) }
