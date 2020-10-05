71a6e3d362244123b2a74cabe94e63cc426a23e5
Tony Johnson <tony@tosp.net.au>
2020-10-05 14:51:34 +1100

    Expand raws to include mobs, weapons, traps

--- server/src/raws/mod.rs
+++ server/src/raws/mod.rs
@@ -1,10 +1,28 @@
 use std::fs::File;
-use super::{MyEntities};
+use super::{Raws, RawEntity};
+use roguelike_common::*;
 
 use ron::de::from_reader;
 
 pub fn load_raws() {
-    let file = File::open("raws/spawns.ron").expect("Cannot open file");
-    let entities: MyEntities = from_reader(file).expect("Cannot read from file");
-    println!("{:?}", entities);
+    let paths = vec![
+        "raws/mobs.ron",
+        "raws/items.ron",
+        "raws/weapons.ron",
+        "raws/traps.ron",
+    ];
+
+    let mut master_raws = Raws::new();
+
+    for path in &paths {
+        let file = File::open(path).expect("Cannot open file");
+        let raws: Raws = from_reader(file).expect("Cannot read from file");
+        master_raws.concat(raws);
+    }
+
+    // testing
+    let ent: Option<RawEntity> = master_raws.entities.into_iter().find(|e| e.code.code == ITEM_HEALTH_POTION);
+    if let Some(e) = ent {
+        println!("WC {:?}", e);
+    }
 }
