49114c85f2ed5b3e1cb8733774fd8d622946f8d5
Tony Johnson <tony@tosp.net.au>
2020-10-15 19:55:48 +1100

    Simplify GameLog and Particles code

--- server/src/gamelog.rs
+++ server/src/gamelog.rs
@@ -3,27 +3,23 @@ use roguelike_common::*;
 
 pub struct GameLog {
     pub logs: Vec<Vec<i32>>,
-    pub has_log: bool,
 }
 
 impl GameLog {
     pub fn new() -> Self {
         Self {
             logs: vec![vec![LogType::System as i32, 0]],
-            has_log: true,
         }
     }
 
     pub fn add_log(&mut self, log: Vec<i32>) {
         self.logs.push(log);
-        self.has_log = true;
     }
 
     pub fn get_logs(&mut self) -> Option<Value> {
-        if self.has_log {
+        if self.logs.len() > 0 {
             let logs = serde_json::to_value(&self.logs).unwrap();
             self.logs.clear();
-            self.has_log = false;
             Some(logs)
         } else {
             None
