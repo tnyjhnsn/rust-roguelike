5f036f0bfe68ea353aefb65eb7af2a732476b5a3
Tony Johnson <tony@tosp.net.au>
2020-10-30 19:41:20 +1100

    Distance culling AI

--- server/src/ai/initiative_system.rs
+++ server/src/ai/initiative_system.rs
@@ -7,6 +7,7 @@ use crate:: {
     GameState,
     RandomNumberGenerator,
     PlayerEntity,
+    PlayerPosition,
 };
 
 pub struct InitiativeSystem {}
@@ -20,27 +21,33 @@ impl<'a> System<'a> for InitiativeSystem {
         WriteExpect<'a, RandomNumberGenerator>,
         ReadStorage<'a, Attributes>,
         WriteExpect<'a, GameState>,
-        ReadExpect<'a, PlayerEntity>
+        ReadExpect<'a, PlayerEntity>,
+        ReadExpect<'a, PlayerPosition>
     );
 
     fn run(&mut self, data: Self::SystemData) {
         let (mut initiatives, positions, mut turns, entities, mut rng,
-             attributes, mut game_state, player_entity) = data;
+             attributes, mut game_state, player_entity, ppos) = data;
 
         if *game_state != GameState::Ticking { return; }
 
         turns.clear();
 
-        for (entity, initiative, _p) in (&entities, &mut initiatives, &positions).join() {
+        for (entity, initiative, pos) in (&entities, &mut initiatives, &positions).join() {
             initiative.current -= 1;
             if initiative.current < 1 {
-                let myturn = true;
+                let mut myturn = true;
                 initiative.current = rng.roll_dice(1, 6, 6);
                 if let Some(attr) = attributes.get(entity) {
                     initiative.current -= attr.quickness.bonus;
                 }
                 if entity == *player_entity {
                     *game_state = GameState::Waiting;
+                } else {
+                    let d = ppos.position.distance(*pos);
+                    if d > 20.0 {
+                        myturn = false;
+                    }
                 }
                 if myturn {
                     turns.insert(entity, MyTurn{}).expect("Unable to insert turn");
